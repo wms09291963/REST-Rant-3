ab228a6bb3f8758240c4e1ce51f2818ebe2b7f4c
Smith <william.smith@bankerslife.com>
2022-09-09 12:17:02 -0400

    adding 404 route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -5,7 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.get('*', (req, res)=> {
+    res.status(404).send('<h1>404 Page</h1>')
+})
 
 
 
-app.listen(3000)
+
+app.listen(process.env.PORT)
